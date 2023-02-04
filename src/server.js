f5689ed0b6a736b359d9f07c59cf25c0269fd5af
Jehwan-Kwak <jehwane@naver.com>
2023-02-05 01:55:45 +0900

    after 3.11

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const handleHome = (req, res) => res.send("I love middlewares.");
+
+app.use(logger);
+app.get("/", logger, handleHome);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening)
