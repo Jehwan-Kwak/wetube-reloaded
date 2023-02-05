a01cb1862f35830fad2e9c0746112c94b9ff0006
Jehwan-Kwak <jehwane@naver.com>
2023-02-05 16:09:10 +0900

    after #4.5

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouster";
+import videoRouter  from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => res.send("I love middlewares.");
-
 app.use(logger);
-app.get("/", logger, handleHome);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
