4d53570f96ba272794622a12aece169ee405b12d
Jehwan-Kwak <jehwane@naver.com>
2023-02-06 01:07:25 +0900

    after #5.5

---
+++ README.md
@@ -0,0 +1,19 @@
+#  Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See user 
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile 
+
+
+/videos/:id -> See video
+/videos/:id/edit -> Edit video
+/videos/upload -> Upload video
+/videos/:id/delete -> Delete video
+
+ 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,9 @@
-export const trending = (req,res) => res.send("Home Page Videos");
-export const edit = (req,res) => res.send("Edit Video");
-export const watch = (req,res) => res.send("Watch Video");
+export const trending = (req,res) => res.render("home", {pageTitle : "Home"});
+export const edit = (req,res) => res.render("edit");
+export const see = (req,res) => res.render("watch");
+export const search = (req,res) => res.send("Search");
+export const upload = (req,res) => res.send("Upload");
+export const deleteVideo = (req,res) => {
+    console.log(req.params);
+    return res.send("Delete Video");
+}
