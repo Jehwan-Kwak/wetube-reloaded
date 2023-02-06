7632597d7024f23760817c9f32bbc07fc4b1411a
Jehwan-Kwak <jehwane@naver.com>
2023-02-07 01:14:12 +0900

    after #6.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,37 @@
-export const trending = (req,res) => res.render("home", {pageTitle : "Home"});
+let videos = [
+    {
+        title:"First video",
+        rating:2,
+        comments:18,
+        createdAt:"3 minutes ago",
+        views: 1,
+        id: 1, 
+    },
+    {
+        title:"Second video",
+        rating:2,
+        comments:18,
+        createdAt:"3 minutes ago",
+        views: 59,
+        id: 2, 
+    },
+    {
+        title:"Third video",
+        rating:2,
+        comments:18,
+        createdAt:"3 minutes ago",
+        views: 59,
+        id: 3, 
+    },
+]
+
+export const trending = (req,res) => res.render("home", {pageTitle : "Home", videos})
 export const edit = (req,res) => res.render("edit");
-export const see = (req,res) => res.render("watch");
+export const watch = (req,res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle:`Watching ${video.title}`, video});
+};
 export const search = (req,res) => res.send("Search");
 export const upload = (req,res) => res.send("Upload");
-export const deleteVideo = (req,res) => {
-    console.log(req.params);
-    return res.send("Delete Video");
-}
+export const deleteVideo = (req,res) => res.send("Delete Video");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -2,7 +2,6 @@ import express from "express";
 import { join, login } from "../controllers/userController";
 import { trending, search } from "../controllers/videoController";
 
-
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
