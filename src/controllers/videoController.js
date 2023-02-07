14d53fa16f8dcb40db8744c2685b13c330eb8f13
Jehwan-Kwak <jehwane@naver.com>
2023-02-08 02:14:16 +0900

    after #6.7

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,12 +26,37 @@ let videos = [
 ]
 
 export const trending = (req,res) => res.render("home", {pageTitle : "Home", videos})
-export const edit = (req,res) => res.render("edit");
 export const watch = (req,res) => {
     const { id } = req.params;
     const video = videos[id-1];
     return res.render("watch", {pageTitle:`Watching ${video.title}`, video});
 };
-export const search = (req,res) => res.send("Search");
-export const upload = (req,res) => res.send("Upload");
-export const deleteVideo = (req,res) => res.send("Delete Video");
+export const getEdit = (req,res) => {
+    const {id} = req.params;
+    const video = videos[id-1];
+    return res.render("edit", {pageTitle:`Editing : ${video.title}`, video});
+}; 
+export const postEdit = (req,res) => {
+    const {id} = req.params;
+    const {title} = req.body;
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req,res) => {
+    return res.render("upload", {pageTitle: "Upload Video"});
+};
+
+export const postUpload = (req,res) => {
+    const {title} = req.body;
+    const newVideo = {
+        title,
+        rating:0,
+        comments:1,
+        createdAt:"Just now",
+        views: 1,
+        id: videos.length +1,     
+    };
+    videos.push(newVideo);
+    return res.redirect("/");
+};
