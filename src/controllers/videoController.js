20781a0a424bede72e26e4dd5ff2f21684db8fef
Jehwan-Kwak <jehwane@naver.com>
2023-02-09 00:24:29 +0900

    after #6.17

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,45 +1,20 @@
-let videos = [
-    {
-        title:"First video",
-        rating:2,
-        comments:18,
-        createdAt:"3 minutes ago",
-        views: 1,
-        id: 1, 
-    },
-    {
-        title:"Second video",
-        rating:2,
-        comments:18,
-        createdAt:"3 minutes ago",
-        views: 59,
-        id: 2, 
-    },
-    {
-        title:"Third video",
-        rating:2,
-        comments:18,
-        createdAt:"3 minutes ago",
-        views: 59,
-        id: 3, 
-    },
-]
+import Video from "../models/Video";
 
-export const trending = (req,res) => res.render("home", {pageTitle : "Home", videos})
-export const watch = (req,res) => {
+export const home = async (req,res) => {
+    const videos = await Video.find({});
+    return res.render("home", {pageTitle:"Home", videos});
+};
+    export const watch = (req,res) => {
     const { id } = req.params;
-    const video = videos[id-1];
-    return res.render("watch", {pageTitle:`Watching ${video.title}`, video});
+    return res.render("watch", {pageTitle:`Watching`});
 };
 export const getEdit = (req,res) => {
     const {id} = req.params;
-    const video = videos[id-1];
-    return res.render("edit", {pageTitle:`Editing : ${video.title}`, video});
+    return res.render("edit", {pageTitle:`Editing`});
 }; 
 export const postEdit = (req,res) => {
     const {id} = req.params;
     const {title} = req.body;
-    videos[id-1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -47,16 +22,19 @@ export const getUpload = (req,res) => {
     return res.render("upload", {pageTitle: "Upload Video"});
 };
 
-export const postUpload = (req,res) => {
-    const {title} = req.body;
-    const newVideo = {
+export const postUpload = async (req,res) => {
+    try {
+    const {title, description, hashtags} = req.body;
+    await Video.create({
         title,
-        rating:0,
-        comments:1,
-        createdAt:"Just now",
-        views: 1,
-        id: videos.length +1,     
-    };
-    videos.push(newVideo);
+        description,
+        hashtags: hashtags.split(",").map((word) => `#${word}`),
+        });
     return res.redirect("/");
+    } catch(error) {
+        res.render("upload", {
+            pageTitle: "Upload Video", 
+            errorMessage: error._message
+        });
+    }
 };
