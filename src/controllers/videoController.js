8ebdc8b314bc1491ed40541be94a9703f4335090
Jehwan-Kwak <jehwane@naver.com>
2023-02-10 01:17:38 +0900

    after #6.21

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,17 +4,35 @@ export const home = async (req,res) => {
     const videos = await Video.find({});
     return res.render("home", {pageTitle:"Home", videos});
 };
-    export const watch = (req,res) => {
+    export const watch = async (req,res) => {
     const { id } = req.params;
-    return res.render("watch", {pageTitle:`Watching`});
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404", {pageTitle:`Video not found`});
+    }
+    return res.render("watch", {pageTitle:video.title, video});
 };
-export const getEdit = (req,res) => {
+export const getEdit = async (req,res) => {
     const {id} = req.params;
-    return res.render("edit", {pageTitle:`Editing`});
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404", {pageTitle: "Video not found"});
+    }
+    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
 }; 
-export const postEdit = (req,res) => {
+export const postEdit = async (req,res) => {
     const {id} = req.params;
-    const {title} = req.body;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404", {pageTitle: "Video not found"});
+    }
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word :`#${word}`));
+    await video.save();
     return res.redirect(`/videos/${id}`);
 };
 
@@ -28,8 +46,8 @@ export const postUpload = async (req,res) => {
     await Video.create({
         title,
         description,
-        hashtags: hashtags.split(",").map((word) => `#${word}`),
-        });
+        hashtags: hashtags.split(",").map((word.startsWith("#") ? word :`#${word}`))
+    });
     return res.redirect("/");
     } catch(error) {
         res.render("upload", {
