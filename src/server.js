e0610161c6278b5477d522b5541e689f6c506ea9
Jehwan-Kwak <jehwane@naver.com>
2023-02-14 01:43:47 +0900

    after #7.18

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo"
 import { localsMiddleware } from "./middlewares";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouster";
@@ -14,14 +15,14 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended : true}));
 app.use(session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIES_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl : process.env.DB_URL}),
 }));
 
 app.use((req, res, next) => {
     req.sessionStore.all((error, sessions) => {
-        console.log(sessions);
         next();
     })
 });
