19ff7a51e6690af554a91825587038218b4a42ff
Jehwan-Kwak <jehwane@naver.com>
2023-03-24 21:57:41 +0900

    Update db.js

--- src/db.js
+++ src/db.js
@@ -5,8 +5,6 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
   });
 
 const db = mongoose.connection;
